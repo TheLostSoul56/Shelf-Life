69cdad9f8b40f79a67b8743df5424fadf2131ae5
Angel Onativia <aronativia@student.fullsail.edu>
2025-04-24 22:57:43 -0400

    created back arrow button on Add User Screen to nav back to Home

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -43,6 +44,8 @@ dependencies {
     implementation(libs.navigation.ui)
     implementation(libs.gson)
     testImplementation(libs.junit)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
